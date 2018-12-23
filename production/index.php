40b146c6a2d244d12f9a4bdb2794eba62e47f616
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2018-12-23 12:36:44 -0500

    Added Twig to composer

---
+++ production/index.php
@@ -0,0 +1,24 @@
+<?php
+
+// Get composer stuff.
+require_once '../vendor/autoload.php';
+
+// Setup twig template engine..
+$loader = new Twig_Loader_Filesystem('view/');
+$twig = new Twig_Environment($loader, array('cache' => 'cache/'));
+
+// Page Variables
+$site_name = "Habit Builder";
+
+// Check if Database is installed..
+if(!file_exists ( 'php/db.inc.php' )) {
+    // If not, load install page..
+    $page_name = 'Install';
+    echo $twig->render('install.twig', ['title' =>  $site_name . ' - ' . $page_name ]);
+    exit();
+}
+
+
+
+
+
