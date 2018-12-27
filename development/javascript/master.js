4edf214fb34603a81d9e86c0365f78bbec91b617
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2018-12-27 00:17:10 -0500

    Setup PHP DB Installer

--- development/javascript/master.js
+++ development/javascript/master.js
@@ -1,21 +1,25 @@
 (function($){
 
     // DB Install functions..
-    function submitInstall(){
-        $.post('php/install', $("#install_form").serialize(), function(json_return){
-            json_return = JSON.parse(json_return);
-
-            if(json_return.error){
-                $("#install_form .errorbox").html(json_return.error);
-            }else{
-                location.reload();
-            }
-        });
-    }
-
-    // Setup functions..
     function setupDBInstall(){
-        $("#install_submit_btn").click(submitInstall);
+        $( "#install_form" ).submit(function( event ) {
+            event.preventDefault();
+            $('.errorbox').hide();
+            $.post("php/install.php", $( this ).serialize(), function(json_return){
+                json_return = JSON.parse(json_return);
+
+                if(json_return.error){
+                    if(json_return.sql_user_error){$("#sql_user_error").html(json_return.sql_user_error).slideDown();}
+                    if(json_return.sql_password_error){$("#sql_password_error").html(json_return.sql_password_error).slideDown();}
+                    if(json_return.sql_database_error){$("#sql_database_error").html(json_return.sql_database_error).slideDown();}
+                    if(json_return.admin_username_error){$("#admin_username_error").html(json_return.admin_username_error).slideDown();}
+                    if(json_return.admin_password_error){$("#admin_password_error").html(json_return.admin_password_error).slideDown();}
+                    if(json_return.db_error){$("#db_error").html(json_return.db_error).slideDown();}
+                }else{
+                    //window.location.replace('/');
+                }
+            });
+        });
     }
 
     function init(){
@@ -23,4 +27,5 @@
     }
 
     $(document).ready(init);
-}(jquery));
+}(jQuery));
+
---
+++ production/php/install.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JoshB
+ * Date: 12/26/2018
+ * Time: 11:19 PM
+ */
+
+$json = [];
+$json['error'] = false;
+
+$sql_user = '';
+$sql_password = '';
+$sql_database = '';
+$admin_username = '';
+$admin_password = '';
+
+if(isset($_POST['db_user'])){
+    $sql_user = $_POST['db_user'];
+}
+if(isset($_POST['db_pass'])){
+    $sql_password = $_POST['db_pass'];
+}
+if(isset($_POST['db_name'])){
+    $sql_database = $_POST['db_name'];
+}
+if(isset($_POST['admin_user'])){
+    $admin_username = $_POST['admin_user'];
+}
+if(isset($_POST['admin_pass'])){
+    $admin_password = $_POST['admin_pass'];
+}
+
+if(isset($_POST['db_created'])){
+    $db_created = $_POST['db_created'];
+}else{
+    $db_created = false;
+}
+
+
+if(empty($sql_user)){
+    $json['error'] = true;
+    $json['sql_user_error'] = "Please the username for SQL connection.";
+}
+if(empty($sql_password)){
+    $json['error'] = true;
+    $json['sql_password_error'] = "Please the password for SQL connection.";
+}
+if(empty($sql_database)){
+    $json['error'] = true;
+    $json['sql_database_error'] = "Please the name of the database app will use.";
+}
+
+if(empty($admin_username)){
+    $json['error'] = true;
+    $json['admin_username_error'] = "Please enter the username for admin account.";
+}
+if(empty($admin_password)){
+    $json['error'] = true;
+    $json['admin_password_error'] = "Please create a password for admin account.";
+}
+if($json['error']){
+    echo json_encode($json);
+    exit();
+}
+
+//Create Database
+$admin_role = 2;
+$admin_password = password_hash($admin_password, PASSWORD_DEFAULT);
+    try {
+        if(!$db_created) {
+            // Create Database with local mysql user..
+            $dbh = new PDO("mysql:host=$host", $sql_user, $sql_password);
+            $dbh->exec("CREATE DATABASE `$db`;")
+            or die(print_r($dbh->errorInfo(), true));
+            $dbh = null;
+        }
+
+        //create db.inc.php file for later connections
+        $dbfile = fopen("db.inc.php", "w");
+        $txt = "<?php\n";
+        $txt .= "try {\n";
+        $txt .= "    \$db = new PDO('mysql:host=localhost;dbname=$sql_database', \"$sql_user\", \"$sql_password\");\n";
+        $txt .= "    \$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );//Error Handling\n";
+        $txt .= " } catch (PDOException \$e) {\n";
+        $txt .= "    print \"Error!: \" . \$e->getMessage() . \"<br/>\";\n";
+        $txt .= "    die();\n";
+        $txt .= "}\n";
+        $txt .= "?>\n";
+        fwrite($dbfile, $txt);
+        fclose($dbfile);
+
+        // Reconnect with new user..
+        $db = new PDO("mysql:dbname=$sql_database;host=localhost", $sql_user, $sql_password);
+        $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );//Error Handling
+
+        // Create Tables..
+        // Users..
+        $sql ="CREATE table users(
+             id INT( 11 ) AUTO_INCREMENT PRIMARY KEY,
+             username VARCHAR( 50 ) NOT NULL,
+             password VARCHAR( 250 ) NOT NULL,
+             role INT( 2 ) NOT NULL);" ;
+        $db->exec($sql);
+
+        // Habits..
+        $sql ="CREATE table habits(
+             id INT( 11 ) AUTO_INCREMENT PRIMARY KEY,
+             user INT( 11 ) NOT NULL,
+             verify INT( 11 ) NOT NULL,
+             title VARCHAR( 50 ) NOT NULL,
+             decription VARCHAR( 250 ) NOT NULL,
+             type INT( 2 ) NOT NULL,
+             level INT( 2 ) NOT NULL,
+             streak INT( 5 ) NOT NULL,
+             lastsuccess TIMESTAMP NOT NULL);" ;
+        $db->exec($sql);
+
+        /*
+        // Settings..
+        $sql ="CREATE table settings(
+             user INT( 11 ) PRIMARY KEY,
+             favorites VARCHAR( 150 ));" ;
+        $db->exec($sql);
+        */
+
+
+        // Admin..
+        // Add Admin to users, settings and Admin Tables..
+        $stmt = $db->prepare("INSERT INTO users (username, password, role) VALUES (?, ?, ?)");
+        $stmt->bindParam(1, $admin_username);
+        $stmt->bindParam(2, $admin_password);
+        $stmt->bindParam(3, $admin_role);
+        $stmt->execute();
+
+        /*
+        $user_id = 1;
+        $fav = "";
+        $stmt = $db->prepare("INSERT INTO settings (user, favorites) VALUES (?, ?)");
+        $stmt->bindParam(1, $user_id);
+        $stmt->bindParam(1, $fav);
+        $stmt->execute();
+        */
+
+        session_start();
+        $_SESSION['username'] = $admin_username;
+        echo json_encode($json);
+    } catch (PDOException $e) {
+        die("DB ERROR: ". $e->getMessage());
+    }
+?>
